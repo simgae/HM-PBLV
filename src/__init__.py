8cf6ed269a03d16131a0e2b7fb3a7f9de80d32a3
Simon Gaertner <simongaertner@gmx.net>
2025-02-06 11:53:58 +0100

    feat: implement YOLOv3 loss function and model architecture with improved bounding box handling

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from src.models.yolo_v3_model import YoloV3Model
 
 if __name__ == '__main__':
     yolo_v3_model = YoloV3Model()
-    #yolo_v3_model.train_model(2)
+    yolo_v3_model.train_model(2)
     #yolo_v3_model.evaluate_model()
 
-    yolo_v3_model._evaluate_image('../data/image.jpeg')
+    #yolo_v3_model._evaluate_image('../data/image.jpeg')
